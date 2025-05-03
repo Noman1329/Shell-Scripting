ec6bb184b5aafb9738f6ca703ad9907742696b8f
NOMAN AKRAM <NOMANAKRAM@CARECLOUD.COM>
2025-05-03 05:40:35 +0500

    Create multiple users

--- User Automation.sh
+++ User Automation.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-
+#Check and create user if not available
 if [ $# -gt 0 ]; then
-    USER=$1
-    EXISTED_USER=$(cat /etc/passwd | grep -E $USER | awk -F ":" '{print $1}')
-    if [ "${USER}" = "${EXISTED_USER}" ]; then
-        echo "User name with ["$USER"] already exist, provide another username!"
-    else
-        sudo useradd -m $USER --shell /bin/bash
-        echo "User ["$USER"] has been created!"
-    fi
+    for USER in $@; do #to add multiple user at a time
+        # USER=$1
+        EXISTED_USER=$(cat /etc/passwd | grep -E $USER | awk -F ":" '{print $1}')
+        if [ "${USER}" = "${EXISTED_USER}" ]; then
+            echo "User name with ["$USER"] already exist, provide another username!"
+        else
+            sudo useradd -m $USER --shell /bin/bash
+            echo "User ["$USER"] has been created!"
+        fi
+    done
 else
     echo "Please provide a valid username to continue!"
 fi
